b5178f8d10073ea8ae5780cdfc8029350d8a0b6a
Austin Connor <aconnor@allophone.net>
2021-08-10 08:57:47 -0500

    wip

--- test.js
+++ test.js
@@ -9,9 +9,14 @@ var expect = chai.expect
 const testCaseInput1 = {
   add: "ADD foo bar",
   addMultipleValues: "ADD foo bar baz",
+  allMembers: "ALLMEMBERS",
   clear: "CLEAR",
   keys: "KEYS",
+  keyExists: "KEYEXISTS foo",
+  items: "ITEMS",
+  memberExists: "MEMBEREXISTS foo bar",
   members: "MEMBERS foo",
+  remove: "REMOVE foo bar",
   removeAll: "REMOVEALL foo"
 }
 
@@ -19,41 +24,60 @@ describe('handleInput()', () => {
   it('ADD', () => {
     const outputHash = { 'foo': ['bar' ] }
     const addReturn = "Added"
-    expect(assert(addReturn, handleInput(testCaseInput1.add, outputHash)))
+    expect(handleInput(testCaseInput1.add, outputHash)).to.be.equal(addReturn)
   })
 
-  it('MEMBERS', () => {
-    const membersReturn = ""
-    expect(assert(membersReturn, handleInput(testCaseInput1.members, outputHash)))
+  it('MEMBERS foo', () => {
+    const outputHash = { 'foo': [ 'bar' ]}
+    const membersReturn = [ 'bar' ]
+    expect(handleInput(testCaseInput1.members, outputHash)[0]).to.be.equal(membersReturn[0])
   })
 
   it('KEYS', () => {
-    const keysReturn = ""
-    expect(assert(keysReturn, handleInput(testCaseInput1.keys, outputHash)))
+    const outputHash = {}
+    const keysReturn = "empty set"
+    expect(handleInput(testCaseInput1.keys, outputHash)).to.be.equal(keysReturn)
   })
 
-  it('should ADD key with multiple values', () => {
-    const outputHash = { 'foo': ['bar', 'baz' ] }
-    expect(assert(outputHash, handleInput(testCaseInput1.addMultipleValues, outputHash)))
+  it('REMOVE foo bar', () => {
+    const outputHash = {}
+    const removeReturn = "ERROR, member does not exist"
+    expect(handleInput(testCaseInput1.remove, outputHash)).to.be.equal(removeReturn)
   })
 
-  it('should return MEMBERS of foo key', () => {
-    const outputHash = { 'foo': ['ice', 'dog'] }
-    expect(assert(outputHash, handleInput(testCaseInput1.members, outputHash)))
+  it('REMOVEALL foo', () => {
+    const outputHash = {}
+    const removeAllReturn = "ERROR, key does not exist"
+    expect(handleInput(testCaseInput1.remove, outputHash)).to.be.equal(removeAllReturn)
   })
 
-  it('should return KEYS of in-memory dictionary', () => {
-    const outputHash = { 'foo': ['run', 'dmc'] }
-    expect(assert(outputHash, handleInput(testCaseInput1.keys, outputHash)))
+  it('CLEAR', () => {
+    const outputHash = {}
+    const clearReturn = "Cleared"
+    expect(handleInput(testCaseInput1.clear, outputHash)).to.be.equal(clearReturn)
   })
 
-  it('should CLEAR the in-memory dictionary', () => {
-    const outputHash = { 'foo': ['idk', 'dos'] }
-    expect(assert(outputHash, handleInput(testCaseInput1.clear, outputHash)))
+  it('KEYEXISTS foo', () => {
+    const outputHash = {}
+    const keyExistsReturn = "false"
+    expect(handleInput(testCaseInput1.keyExists, outputHash)).to.be.equal(keyExistsReturn)
   })
 
-  // it('should REMOVEALL values of and the key of foo', () => {
-  //   const outputHash = { 'foo': ['doo', 'bop'] }
-  //   expect(assert(outputHash, handleInput(testCaseInput1.removeAll, outputHash)))
-  // })
+  it('MEMBEREXISTS foo bar', () => {
+    const outputHash = {}
+    const memberExistsReturn = "false"
+    expect(handleInput(testCaseInput1.memberExists, outputHash)).to.be.equal(memberExistsReturn)
+  })
+
+  it('ALLMEMBERS', () => {
+    const outputHash = {}
+    const allMembersReturn = "empty set"
+    expect(handleInput(testCaseInput1.allMembers, outputHash)).to.be.equal(allMembersReturn)
+  })
+
+  it('ITEMS', () => {
+    const outputHash = {}
+    const itemsReturn = "empty set"
+    expect(assert(itemsReturn, handleInput(testCaseInput1.items, outputHash)))
+  })
 })
